0b6e92bb125a5dd585204dc1af26e67039a530f3
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-02-18 17:35:13 -0500

    Clase Motor

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ class Asiento():
         coloresValidos = ["rojo","verde","amarillo","negro","blanco"]
         if (color in coloresValidos):
             self.color = color
+
+class Motor():
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro (self, registro):
+        self.registro = registro
+
+    def asignarTipo (self, tipo):
+        if (tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
