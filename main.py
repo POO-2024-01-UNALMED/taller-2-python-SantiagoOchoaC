34ea45fdd229fc428e6df5fcc6c22b7a5f68bc36
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-02-18 17:36:39 -0500

    Clase Auto

--- main.py
+++ main.py
@@ -21,3 +21,30 @@ class Motor():
     def asignarTipo (self, tipo):
         if (tipo == "electrico" or tipo == "gasolina"):
             self.tipo = tipo
+
+class Auto():
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos (self):
+        numeroAsientos = 0
+        for i in self.asientos:
+            if (i != None):
+                numeroAsientos += 1
+        return numeroAsientos
+
+
+    def verificarIntegridad (self):
+        if (self.registro == self.motor.registro):
+            for i in self.asientos:
+                if (i != None and i.registro != self.registro):
+                    return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
